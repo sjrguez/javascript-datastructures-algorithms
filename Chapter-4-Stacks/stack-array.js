3bfa35fb24a770cfc87149557eb0d2c9a810f7d4
Starlin Rodriguez <starlinr82@gmail.com>
2020-01-23 00:09:01 -0400

    Adding the 'toString' method. Crearing a function decimalToBinary

--- Chapter-4-Stacks/stack-array.js
+++ Chapter-4-Stacks/stack-array.js
@@ -21,10 +21,21 @@ class Stack { // Using the LIFO principle, the last in is the first out
         return this.items.length === 0;
     }
 
+
+
     size() {
         return this.items.length;
     }
-    
+
+
+    toString() {
+	if (this.isEmpty()) {
+		return '';
+	}
+	return this.items.toString()
+
+    }
+
     clear() {
         this.items = [];
     }
@@ -48,5 +59,36 @@ stack.push(15);
 
 
 stack.pop();
-stack.pop();
 console.log(`The new stack's size is: `,stack.size());
+
+console.log(stack.toString(), "toString method")
+
+// Converting from decimal to binary 
+const decimalToBinary = (decimalNumber) => {
+	const stack = new Stack();
+	let decimalNum = decimalNumber;
+	binaryString = ''
+	while(decimalNum > 0 ) {
+
+		const floorNumber = Math.floor(decimalNum % 2);
+		stack.push(floorNumber);
+		decimalNum =  Math.floor(decimalNum / 2);
+	}
+
+	while(!stack.isEmpty()) {
+		binaryString += stack.pop()
+	}
+
+	return binaryString;
+}
+
+
+
+
+const binary = decimalToBinary(20)
+const binary2 = decimalToBinary(233)
+const binary3 = decimalToBinary(1000)
+
+console.log(`20 to binary is: ${binary}`)
+console.log(`233 to binary is: ${binary2}`)
+console.log(`1000 to binary is: ${binary3}`)
